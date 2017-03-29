a138c8b0ae3cef5e0d8e4b5f12a43ea046f4bc51
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-29 10:04:20 -0600

    Add request for stations and VCR to service spec

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,2 +1,15 @@
 class NRELService
+  def initialize
+    @nrel_key = ENV['NREL_KEY']
+  end
+
+  def get_stations_by_zipcode(zipcode)
+    stations = HTTParty.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{@nrel_key}&location=#{zipcode}&radius=6.0&status=E&fuel_type=ELEC,LPG&limit=10")
+    parse(stations)
+    byebug
+  end
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 describe "NRELSerivce" do
   describe "get_stations_by_zipcode(zipcode)" do
     it "returns stations within 6.0 miles of the zipcode" do
-      service = NRELService.new
-      stations = service.get_stations_by_zipcode(85701)
-      expect(stations.first[:access_days_time]).to eq("MON: 24 hours | TUE: 24 hours | WED: 24 hours | THU: 24 hours | FRI: 24 hours | SAT: 24 hours | SUN: 24 hours")
-      expect(stations.first[:fuel_type_code]).to eq("ELEC")
-      expect(stations.first[:station_name]).to eq("Pennington St. Garage")
-      expect(stations.first[:distance]).to eq("0.58563")
-      expect(stations.first[:street_address]).to eq("110 E. Pennington")
-      expect(stations.count).to eq(10)
+      VCR.use_cassette("services/get_stations_by_zipcode") do
+        service = NRELService.new
+        stations = service.get_stations_by_zipcode(85701)
+        expect(stations.first[:access_days_time]).to eq("MON: 24 hours | TUE: 24 hours | WED: 24 hours | THU: 24 hours | FRI: 24 hours | SAT: 24 hours | SUN: 24 hours")
+        expect(stations.first[:fuel_type_code]).to eq("ELEC")
+        expect(stations.first[:station_name]).to eq("Pennington St. Garage")
+        expect(stations.first[:distance]).to eq("0.58563")
+        expect(stations.first[:street_address]).to eq("110 E. Pennington")
+        expect(stations.count).to eq(10)
+      end
     end
   end
 end
