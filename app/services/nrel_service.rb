388f295854bd000ec3c5df9ec9d42f777e8662fe
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-29 10:06:03 -0600

    Update get_stations_by_zipcode, test passes

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -5,8 +5,8 @@ class NRELService
 
   def get_stations_by_zipcode(zipcode)
     stations = HTTParty.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{@nrel_key}&location=#{zipcode}&radius=6.0&status=E&fuel_type=ELEC,LPG&limit=10")
-    parse(stations)
-    byebug
+    parse(stations)[:fuel_stations]
+    # byebug
   end
 
   def parse(response)
--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -9,7 +9,7 @@ describe "NRELSerivce" do
         expect(stations.first[:access_days_time]).to eq("MON: 24 hours | TUE: 24 hours | WED: 24 hours | THU: 24 hours | FRI: 24 hours | SAT: 24 hours | SUN: 24 hours")
         expect(stations.first[:fuel_type_code]).to eq("ELEC")
         expect(stations.first[:station_name]).to eq("Pennington St. Garage")
-        expect(stations.first[:distance]).to eq("0.58563")
+        expect(stations.first[:distance]).to eq(0.58563)
         expect(stations.first[:street_address]).to eq("110 E. Pennington")
         expect(stations.count).to eq(10)
       end
